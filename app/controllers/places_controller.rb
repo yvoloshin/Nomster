2113566acb3037c1cfe8efc9e987e10fcf4a9108
Yury Voloshin <yvoloshin01@gmail.com>
2015-05-15 18:45:38 +0000

    Lesson 14

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,6 +7,17 @@ class PlacesController < ApplicationController
 		@places=Place.new
 	end
 
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
+	end
+
 
 end
 		
