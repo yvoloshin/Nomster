0543622a7e307904a11a6c17b99a174470bcd645
Yury Voloshin <yvoloshin01@gmail.com>
2015-06-07 06:28:28 +0000

    TDD 9

---
+++ test/factories.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  factory :user do
+  	email "email@gmail.com"
+  	password "password"
+    password_confirmation "password"
+  end 
+  factory :place do
+    name "Lulu's Bakery"
+    description "Try the cupcakes"
+    latitude(42.3631519)
+    longitude(-71.056098)
+    association :user
+  end
+  factory :comment do
+  	message "I love this place!"
+    rating "1_star"
+    association :user
+    association :place
+  end
+end
