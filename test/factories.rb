0a18ecfb314088038f321a126275db53102c04eb
Yury Voloshin <yvoloshin01@gmail.com>
2015-06-07 17:30:50 +0000

    TDD 9-3

--- test/factories.rb
+++ test/factories.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
  
   factory :user do
-    email "email@email.com"
+    sequence :email do |n|
+      "yoloman#{n}@gmail.com"
+    end
     password "omglolhahaha"
     password_confirmation "omglolhahaha"
   end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,10 +3,11 @@ require 'test_helper'
 class CommentTest < ActiveSupport::TestCase
    test "humanized_rating" do  	    	 
      user = FactoryGirl.create(:user)
-     sign_in user
-
+     puts user.inspect
      place = FactoryGirl.create(:place)
+     puts user.inspect
      comment = FactoryGirl.create(:comment)
+     puts user.inspect
      expected = 'one star'
      actual = comment.humanized_rating
      assert_equal expected, actual
