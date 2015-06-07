b686162e24e4e8811c2c7b98f0bfb365d388bf93
Yury Voloshin <yvoloshin01@gmail.com>
2015-06-07 22:18:45 +0000

    TDD 9-5

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -6,11 +6,14 @@ class CommentsControllerTest < ActionController::TestCase
   	sign_in user
 
 	place = FactoryGirl.create(:place)
-	place.inspect
 	comment = FactoryGirl.create(:comment)
-	comment.inspect
-  	post :create, :place_id => place.id, :comment => comment
-  	assert_response :success
-  	assert_redirected_to place_path(place)
-    end
+		assert_difference 'Comment.count' do
+	  	post :create, :place_id => place.id, :comment => {
+	        :rating  => '1_star',
+	        :message => 'This place is awful'
+	    } 	
+	  end
+  assert_equal 1, user.comments.count
+  assert_redirected_to place_path(place)
+	end
 end
